d851da5c72d4c3a021c3bf4e271b869d49163efe
NoahDons <nedons@cps.edu>
2024-10-16 11:24:40 -0500

    Added baseball players

--- players.py
+++ players.py
@@ -6,22 +6,55 @@ player_db: List[Tuple[str, int, List[str], List[str]]] = [
         "Lebron James",
         22,
         ["Basketball"],
-        ["teams here"],
+        ["Cavaliers", "Heat", "Lakers"],
     ),
     
 
-     (
+    (
         "Lionel Messi",
         20,
         ["Soccer"],
-        ["teams here"],
+        ["Argentina", "Barcelona"],
     ),
 
      (
         "Michael Phelps",
         24,
         ["Swimming"],
-        ["teams here"],
+        ["USA"],
+    ),
+    (
+        "Henry Aaron",
+        22,
+        ["Baseball"],
+        ["Braves"],
+    ),
+
+    (
+        "Luis Aparicio",
+        17,
+        ["Baseball"],
+        ["White Sox"],
+    ),
+
+    (
+        "Roberto Alomar",
+        16,
+        ["Baseball"],
+        ["Padres", "Blue Jays", "Orioles", "Indians"],
+    ),
+
+    (
+        "Jose Altuve",
+        13,
+        ["Baseball"],
+        ["Astros"],
+    ),
+    (
+        "Gary Carter",
+        18,
+        ["Baseball"],
+        ["Expos", "Mets"],
     ),
 
 
